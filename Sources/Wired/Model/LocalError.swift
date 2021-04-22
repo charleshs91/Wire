cb8080dbb5856f2158f85d6d8d2c5fe4d6129c19
Charles Hsieh <charlous167@gmail.com>
2021-04-22 16:10:24 +0800

    Improve documentation of LocalError

--- Sources/Wired/Model/LocalError.swift
+++ Sources/Wired/Model/LocalError.swift
@@ -1,21 +1,30 @@
 import Foundation
 
 public enum LocalError: LocalizedError {
-    /// The URL string is invalid.
+
+    /// The URL string is invalid. The associated value represents the URL string,
+    /// which is compared upon evaluating equality.
     case invalidURLString(String)
-    /// Error from `URLSession`
-    case sessionError(Error)
+
+    /// Error from `URLSession`. The `error` is ignored upon evaluating equality.
+    case sessionError(_ error: Error)
+
     /// No response from server
     case noResponse
-    /// The response is not HTTP.
+
+    /// The response is not HTTP. The `response` is taken into equality evaluation.
     case notHttpResponse(response: URLResponse)
-    /// HTTP response with status code other than 200
+
+    /// HTTP response with status code other than 200. Only the `code` is taken into equality evaluation.
     case httpStatus(code: Int, data: Data?)
+
     /// The response (200 OK) does not contain data.
     case noData
-    /// Error from `RequestBuildable.buildRequest()`
+
+    /// Error from `RequestBuildable.buildRequest()`. The `error` is ignored upon evaluating equality.
     case requestFactoryError(Error)
-    /// Error from `ResponseConvertible.convert(data:)`
+
+    /// Error from `ResponseConvertible.convert(data:)`. The `error` is ignored upon evaluating equality.
     case responseConversionError(Error)
 
     public var errorDescription: String? {
@@ -46,11 +55,12 @@ extension LocalError: Equatable {
             return leftString == rightString
         case (.sessionError, .sessionError),
              (.noResponse, .noResponse),
-             (notHttpResponse, .notHttpResponse),
              (.noData, .noData),
              (.requestFactoryError, .requestFactoryError),
              (.responseConversionError, .responseConversionError):
             return true
+        case (.notHttpResponse(let leftResponse), .notHttpResponse(let rightResponse)):
+            return leftResponse == rightResponse
         case (.httpStatus(let leftCode, _), .httpStatus(let rightCode, _)):
             return leftCode == rightCode
         default:
